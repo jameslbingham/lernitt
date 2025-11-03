df67007a7fd6808aad5abf01fe5e9799eb4d32c4
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2025-11-03 10:43:08 +0100

    Delete server-new directory

--- server-new/utils/notify.js
+++
@@ -1,7 +0,0 @@
-const Notification = require('../models/Notification');
-
-async function notify(user, type, title, message = '', data = {}) {
-  return Notification.create({ user, type, title, message, data });
-}
-
-module.exports = { notify };
--- server-new/utils/policies.js
+++
@@ -1,7 +0,0 @@
-export const canReschedule = (lesson, now = new Date()) => {
-  return lesson && lesson.startTime && (lesson.startTime - now) / 36e5 >= 24;
-};
-
-export const refundAllowed = () => {
-  return false; // Only if required by law
-};
