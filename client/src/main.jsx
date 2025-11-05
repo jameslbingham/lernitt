a74d78e3ac2e423607143a5cb6e34fa9c88aa78c
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2025-11-05 22:26:04 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,16 +4,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ToastProvider } from "@/components/ui/ToastProvider.jsx";
 import { ConfirmProvider } from "./components/ui/ConfirmProvider";
+import { AuthProvider } from "@/hooks/useAuth";
 
 const mockMode = import.meta.env.VITE_MOCK === "1";
 console.log("Lernitt booting, mockMode =", mockMode);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ToastProvider>
-      <ConfirmProvider>
-        <App mockMode={mockMode} />
-      </ConfirmProvider>
-    </ToastProvider>
+    <AuthProvider>
+      <ToastProvider>
+        <ConfirmProvider>
+          <App mockMode={mockMode} />
+        </ConfirmProvider>
+      </ToastProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
