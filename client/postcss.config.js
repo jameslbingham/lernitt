faa357f596cf1446b263e59ebcb3c09424a12333
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2025-12-17 13:04:47 +1100

    Update postcss.config.js

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,7 @@
 // client/postcss.config.js
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwindcss(), autoprefixer()],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
