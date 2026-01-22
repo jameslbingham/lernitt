a22f16994207fe804abcbe8a2c907caff59b1817
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2026-01-22 12:52:26 +1100

    Update reviews.js

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middleware/auth");
+const { auth } = require("../middleware/auth");
 const Review = require("../models/Review");
 const Lesson = require("../models/Lesson");
 const mongoose = require("mongoose");
